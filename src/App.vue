ef915e7737b16944caf031d91f8bbf3a1a31c6ae
lyd <1526566980@qq.com>
2022-06-22 01:40:23 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <aggregate></aggregate>
+  </div>
+</template>
+
+<script>
+import aggregate from "@/pages/all.vue";
+export default {
+  name: "App",
+  components: {
+    aggregate,
+  },
+};
+</script>
+
+<style>
+@import "./assets/css/reset.css";
+body {
+  font-family: Microsoft YaHei !important;
+}
+div {
+  box-sizing: border-box;
+}
+</style>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <indexNav />
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import indexNav from "@/components/indexNav.vue";
+export default {
+  components: {
+    indexNav,
+  },
+};
+</script>
+
+<style>
+</style>
